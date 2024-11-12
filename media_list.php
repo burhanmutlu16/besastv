66d32af6b4a6c87d3b571cc13487d2e361346df0
Burhan Mutlu <79896731+burhanmutlu16@users.noreply.github.com>
2024-11-12 09:10:28 +0300

    Add files via upload

---
+++ media_list.php
@@ -0,0 +1,17 @@
+<?php
+// static klasöründeki tüm medya dosyalarını listele
+$mediaFolder = 'static';
+$mediaFiles = [];
+
+// static klasöründe bulunan tüm dosyaları oku
+foreach (scandir($mediaFolder) as $file) {
+    // Resim ve video dosyalarına göre filtrele
+    if (preg_match('/\.(jpg|jpeg|png|mp4)$/i', $file)) {
+        $type = preg_match('/\.(mp4)$/i', $file) ? 'video' : 'image';
+        $mediaFiles[] = ['type' => $type, 'src' => "$mediaFolder/$file"];
+    }
+}
+
+// JSON olarak medya listesini döndür
+header('Content-Type: application/json');
+echo json_encode($mediaFiles);
